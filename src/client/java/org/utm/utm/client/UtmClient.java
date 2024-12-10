dae2d6a4fb885457ebaced4252c6743bd5f4788b
Nic <georgiou200308@hotmail.com>
2024-12-10 16:05:33 +0800

    Initial commit

---
+++ src/client/java/org/utm/utm/client/UtmClient.java
@@ -0,0 +1,10 @@
+package org.utm.utm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UtmClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/utm/utm/Utm.java
@@ -0,0 +1,10 @@
+package org.utm.utm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Utm implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
